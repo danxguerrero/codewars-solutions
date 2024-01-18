0034d8bc8451102b993a7ec2ccbe8786ec4fda46
danxguerrero <danxguerrero.yahoo.com>
2024-01-17 22:39:23 -0500

    Convert a String to a Number! solution

---
+++ go-solutions/kata/stringToNum.go
@@ -0,0 +1,26 @@
+// 8 kyu Convert a String to a Number!
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+// Examples
+// "1234" --> 1234
+// "605"  --> 605
+// "1405" --> 1405
+// "-7" --> -7
+
+package kata
+
+import (
+        "log"
+        "strconv"
+)
+
+
+func StringToNumber(str string) int {
+      n, err := strconv.Atoi(str)
+      if err != nil {
+        log.Fatal(err)
+      }
+      return n
+}

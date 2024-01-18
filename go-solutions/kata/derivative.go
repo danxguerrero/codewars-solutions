7348bafbae054d754c6b39983fa22143509647fd
danxguerrero <danxguerrero@yahpp.com>
2024-01-17 21:57:39 -0500

    Take the derivative solution

---
+++ go-solutions/kata/derivative.go
@@ -0,0 +1,20 @@
+// 8 kyu Take the Derivative
+// This function takes two numbers as parameters, the first number being the coefficient, and the second number being the exponent.
+
+// Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to return an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
+
+// For example:
+
+// derive(7, 8)
+// In this case, the function should multiply 7 and 8, and then subtract 1 from 8. It should output "56x^7", the first number 56 being the product of the two numbers, and the second number being the exponent minus 1.
+
+// derive(7, 8) --> this should output "56x^7" 
+// derive(5, 9) --> this should output "45x^8" 
+
+package kata
+
+import "fmt"
+
+func Derive(coefficient, exponent int) string {
+  return fmt.Sprintf("%dx^%d", coefficient * exponent, exponent - 1)
+}

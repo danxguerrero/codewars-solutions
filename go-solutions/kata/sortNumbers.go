707d0648b3270c736011409dec7b455b09e6db66
danxguerrero <danxguerrero@yahoo.com>
2024-01-21 21:24:53 -0500

    Sort Numbers solution

---
+++ go-solutions/kata/sortNumbers.go
@@ -0,0 +1,21 @@
+//7 kyu Sort Numbers
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+// For example:
+// solution(c(1, 2, 3, 10, 5)) # should return c(1, 2, 3, 5, 10)
+// solution(NULL)              # should return NULL
+
+package kata
+
+import "sort"
+
+func SortNumbers(numbers []int) []int {
+  if len(numbers) > 0 { 
+    sort.Ints(numbers)
+    return numbers
+  }
+  
+  return []int{}
+}
+
+// Realixed after submitting that I don't need both returns. 
+// I could have just returned numbers after the if statement.

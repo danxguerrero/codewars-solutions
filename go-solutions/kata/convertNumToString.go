8a490f90ea1ce3b76020818e0ccfa34f4419c3fe
danxguerrero <danxguerrero@yahoo.com>
2024-01-29 22:31:34 -0500

    Convert a Number to a String! solution

---
+++ go-solutions/kata/convertNumToString.go
@@ -0,0 +1,19 @@
+// 8 kyu Convert a Number to a String!
+
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+package kata
+
+
+import "strconv"
+
+func NumberToString(n int) string {
+  return strconv.Itoa(n)
+}

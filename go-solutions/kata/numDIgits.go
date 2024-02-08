63fe77483e51da9628294832a12ea2aa4218ffce
danxguerrero <danxguerrero@yahoo.com>
2024-02-07 21:24:11 -0500

    Number of Decimal Digits solution

---
+++ go-solutions/kata/numDIgits.go
@@ -0,0 +1,16 @@
+// 7 kyu Number of Decimal Digits
+
+// Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+// All inputs will be valid.
+
+package kata
+
+
+import (
+  "strconv"
+)
+
+func Digits(n uint64) int {
+  	return len(strconv.FormatUint(n, 10))
+}
